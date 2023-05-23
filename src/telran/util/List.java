457758978b960257d2f9ae2a9cd03c99aaa3040b
DimaOstanin <xnadab@gmail.com>
2023-05-23 19:58:44 +0300

    CW #14

--- src/telran/util/List.java
+++ src/telran/util/List.java
@@ -8,9 +8,7 @@ public interface List<T> extends Collection<T> {
 void add(int index, T obj);
 T remove(int index);
 T get(int index) ;
-
-
-
+@SuppressWarnings("unchecked")
 default void sort() {
 	sort((Comparator<T>)Comparator.naturalOrder());
 
@@ -18,28 +16,27 @@ default void sort() {
 void sort(Comparator<T> comp);
 int indexOf(Predicate<T> predicate);
 int lastIndexOf(Predicate<T> predicate);
-
+@Override
  default public boolean remove(T pattern) {
-	  boolean res = false;
-	  int index = indexOf(pattern);
-	  if(index > -1) {
-		  res = true;
-		  remove(index);
-	  }
-	  return res;
- }
-
+	boolean res = false;
+	int index = indexOf(pattern);
+	if (index > -1) {
+		res = true;
+		remove(index);
+	}
+	return res;
+}
+@Override
 default boolean contains(T pattern) {
-	return indexOf(pattern)> -1;
+	return indexOf(pattern) > -1;
 }
 
 default int indexOf(T pattern) {
 	return indexOf(obj -> isEqual(obj, pattern));
 }
-
-
-
 default int lastIndexOf(T pattern) {
 	return lastIndexOf(obj -> isEqual(obj, pattern));
 }
+
+
 }
